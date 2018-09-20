fdc8baaf63cb9d026ddbcabf9ed4dc08c4b82e08
abdelaziz <abdelaziz.iddouch@gmail.com>
2018-09-20 04:43:17 +0100

    first commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { term: '' };
+  }
+
+  render() {
+    return <input
+      value={ this.state.term } 
+      onChange={ event => this.setState({ term: event.target.value }) } />;
+  }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/SearchBar';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyAPq3Q4kRsBSFHwZzVYdmuoXlq-CCm2Cqc';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )};
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<App/>, document.querySelector('.container'));
