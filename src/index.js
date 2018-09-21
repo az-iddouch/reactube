b714d2060a2445b8b668c7c7d42c5b5ad5a8e1b0
abdelaziz <abdelaziz.iddouch@gmail.com>
2018-09-21 21:20:22 +0100

    fetched data from youtube api and displayed it

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SearchBar from './components/SearchBar';
+import React from "react";
+import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
+import SearchBar from "./components/SearchBar";
+import VideoList from "./components/VideoList";
 
-const API_KEY = 'AIzaSyAPq3Q4kRsBSFHwZzVYdmuoXlq-CCm2Cqc';
+const API_KEY = "AIzaSyAPq3Q4kRsBSFHwZzVYdmuoXlq-CCm2Cqc";
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-ReactDOM.render(<App/>, document.querySelector('.container'));
+    this.state = {
+      videos: []
+    };
+
+    YTSearch({ key: API_KEY, term: "surfboards" }, videos =>
+      this.setState({ videos })
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector(".container"));
